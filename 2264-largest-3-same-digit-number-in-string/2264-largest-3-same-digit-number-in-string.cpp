c7b756d21cc249b9563f5a1ff87f367c63402915
Dhruv Nischal <121623050+DhruvNischal@users.noreply.github.com>
2025-02-20 00:52:48 +0530

    Time: 0 ms (100%), Space: 9.1 MB (24.33%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    string largestGoodInteger(string nums) 
-    {
-        string result = "";
-        char ch = 0;
-        for(int i = 0;i< nums.size()-2;i++)
-        {
-            if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2])
-            {
-                if(nums[i] > ch)
-                {
-                    ch = nums[i];
+    string largestGoodInteger(string nums) {
+        string ans = "";
+        
+        for (int i = 0; i < nums.size() - 2; i++) {  
+            if (nums[i] == nums[i + 1] && nums[i] == nums[i + 2]) {  
+                string triplet(3, nums[i]);  // Create a string of three repeating digits
+                
+                if (ans.empty() || ans < triplet) {  
+                    ans = triplet;  // Update ans with the largest triplet found
                 }
             }
         }
-        if(ch == 0)
-        {
-            return result;
-        }
-        for(int i = 0 ;i < 3 ;i++)
-        {
-            result += ch;
-        }
-        return result;
+
+        return ans;
     }
 };
